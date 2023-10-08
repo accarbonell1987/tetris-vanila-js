45872f1518435fc1770ebb67d66c2c4b2e0b6be1
accarbonellweecover <albertocarlos.carbonell@weecover.com>
2023-10-08 13:52:14 -0400

    nextui, tailwind installed

--- react-tetris/src/main.jsx
+++ react-tetris/src/main.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { App } from "./views";
-import "./styles/index.css";
+import "./index.css";
+
+import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<NextUIProvider>
+			<App />
+		</NextUIProvider>
 	</React.StrictMode>
 );
