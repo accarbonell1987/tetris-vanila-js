189b7edd478d2251b00276c1e6183f3f2fd2ae6d
accarbonellweecover <albertocarlos.carbonell@weecover.com>
2023-10-08 19:15:15 -0400

    structure begin

--- react-tetris/src/views/App/App.jsx
+++ react-tetris/src/views/App/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
+import { Image } from "@nextui-org/react";
+
 const App = () => {
-	return <>VITE + NextUI</>;
+	return (
+		<main className="flex flex-col gap-1">
+			<div className="flex flex-row gap-1">
+				<Image src="logo-tetris.png" width={70} alt="Tetris" radius="none" />
+			</div>
+			<div className="flex flex-row gap-1 grow">
+				<article className="flex-none w-14">Left</article>
+				<article className="grow w-14">Game</article>
+				<article className="flex-none w-14">Right</article>
+			</div>
+		</main>
+	);
 };
 
 export default App;
