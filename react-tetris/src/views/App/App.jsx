1fc32b00c7c18966cd5c98c8d659ada4164501cc
accarbonellweecover <albertocarlos.carbonell@weecover.com>
2023-10-09 22:04:11 -0400

    nextui somes adds

--- react-tetris/src/views/App/App.jsx
+++ react-tetris/src/views/App/App.jsx
@@ -1,12 +1,27 @@
 import "./App.css";
 
-import { Image } from "@nextui-org/react";
+import { Image, User, Link } from "@nextui-org/react";
 
 const App = () => {
 	return (
 		<main className="flex flex-col gap-1">
-			<div className="flex flex-row gap-1">
-				<Image src="logo-tetris.png" width={70} alt="Tetris" radius="none" />
+			<div className="flex flex-row gap-1 align-middle">
+				<Image src="logo-tetris.png" width={60} alt="Tetris" radius="none" />
+				<div className="flex flex-row fixed right-0 mr-10">
+					<User
+						name="Alberto Carlos"
+						description={
+							<Link href="https://twitter.com/accarbonell" size="sm" isExternal>
+								@accarbonell
+							</Link>
+						}
+						avatarProps={{
+							src: "https://i.pravatar.cc/150?u=a042581f4e29026704e",
+							isBordered: true,
+							color: "success",
+						}}
+					/>
+				</div>
 			</div>
 			<div className="flex flex-row gap-1 grow">
 				<article className="flex-none w-14">Left</article>
